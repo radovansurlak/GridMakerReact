1b7013ce9a2e10a18110194c9a799da0ef7c011c
radovansurlak <radovansurlak@gmail.com>
2019-04-02 16:17:53 +0100

    added basic controls for ML

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import { render } from 'react-dom';
 import Box from './Box';
 import './style.css';
 
+import brain from 'brain.js';
+
 const GRID_SIZE = 5;
 
 class App extends Component {
@@ -13,12 +15,17 @@ class App extends Component {
       ordered: false,
     }
     this.updateGrid = this.updateGrid.bind(this);
+    this.trainBrain = this.trainBrain.bind(this);
   }
 
   updateGrid(index) {
     this.state.grid[index] ^= 1;
     this.forceUpdate()
     console.log(this.state.grid)
+  }
+
+  trainBrain() {
+    alert('training the brain');
   }
    
 
@@ -29,16 +36,28 @@ class App extends Component {
     boxes.forEach((box, index) => {
       (index % breakpoint == 0) && boxes.splice(index, 0, <div className='line-break'/>);
     })
-    console.log(boxes)
 
     return (
-      <div class="grid-container">
-        {boxes}
+      <main>
+        <div class="grid-container">
+          {boxes}
+        </div>
         <p>{JSON.stringify(this.state.grid)}</p>
-        <input type="checkbox" checked={this.state.ordered} onClick={() => this.setState({ordered: !this.state.ordered})}></input>
-      </div>
+        <label for="ordered-checkbox">Ordered</label><input type="checkbox" id="ordered-checkbox" checked={this.state.ordered} onClick={() => this.setState({ordered: !this.state.ordered})}></input>
+        <br/>
+        <button onClick={() => this.trainBrain()}>Train network</button>
+      </main>
     );
   }
 }
+
+const net = new brain.NeuralNetwork();
+
+net.train([{input: [0, 0], output: [0]},
+           {input: [0, 1], output: [1]},
+           {input: [1, 0], output: [1]},
+           {input: [1, 1], output: [0]}]);
+
+const output = net.run([1, 0]);  // [0.987]
 
 render(<App />, document.getElementById('root'));
