e6130e340da87920f3da6db8c9ab2ad0cec62668
radovansurlak <radovansurlak@gmail.com>
2019-04-02 18:28:45 +0100

    Adding symmetrical and assymetrical patterns

--- index.js
+++ index.js
@@ -9,10 +9,34 @@ const GRID_SIZE = 5;
 
 const net = new brain.NeuralNetwork();
 
-net.train([{input: [0, 0], output: [0]},
-           {input: [0, 1], output: [1]},
-           {input: [1, 0], output: [1]},
-           {input: [1, 1], output: [0]}]);
+const symmetricalPatterns = [
+  [0,0,0,0,0,0,0,1,0,0,0,1,0,1,0,0,0,1,0,0,0,0,0,0,0],
+  [0,0,1,0,0,0,0,1,0,0,1,1,0,1,1,0,0,1,0,0,0,0,1,0,0],
+  [0,0,0,0,0,0,1,1,1,0,0,1,0,1,0,0,1,1,1,0,0,0,0,0,0],
+  [0,0,1,0,0,0,1,1,1,0,1,1,0,1,1,0,1,1,1,0,0,0,1,0,0],
+  [1,1,1,1,1,1,0,0,0,1,1,0,0,0,1,1,0,0,0,1,1,1,1,1,1],
+  [1,0,0,0,1,0,1,0,1,0,0,0,1,0,0,0,1,0,1,0,1,0,0,0,1],
+  [0,0,1,0,0,0,1,0,1,0,1,0,0,0,1,0,1,0,1,0,0,0,1,0,0],
+  [0,0,1,0,0,0,1,1,1,0,1,1,1,1,1,0,1,1,1,0,0,0,1,0,0],
+  [0,0,0,0,0,0,1,1,1,0,0,1,1,1,0,0,1,1,1,0,0,0,0,0,0],
+  [0,1,1,1,0,1,0,0,0,1,1,0,0,0,1,1,0,0,0,1,0,1,1,1,0],
+  [0,1,1,1,0,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0,1,1,1,0],
+]
+
+const asymmetricalPatters = [
+  [0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+  [0,0,0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,1,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1,1,0,0,0],
+
+]
+
+symmetricalPatterns.forEach(pattern => {
+  console.log(net.train({input: pattern, output: [1]}))
+})
 
 const output = net.run([1, 0]);  // [0.987]
 
@@ -35,20 +59,21 @@ class App extends Component {
   }
 
   testBrain() {
+    console.log(this.state.grid);
     const output = net.run(this.state.grid);
-    console.log(output)
+    alert(output);
   }
 
   trainBrain() {
     let output = this.state.symmetrical === true ? 1 : 0;
     let trainingIteration = net.train([{input: this.state.grid, output: [output]}]);
     console.log(trainingIteration)
+    console.log(this.state.symmetrical)
   }
    
 
   render() {
     const breakpoint = Math.sqrt(this.state.grid.length) + 1;
-    console.log(breakpoint)
     let boxes = this.state.grid.map((box, index) => <Box index={index} toggled={this.state.grid[index] == 1} onBoxToggle={this.updateGrid} />)
     boxes.forEach((box, index) => {
       (index % breakpoint == 0) && boxes.splice(index, 0, <div className='line-break'/>);
@@ -59,7 +84,7 @@ class App extends Component {
         <div class="grid-container">
           {boxes}
         </div>
-        <p>{JSON.stringify(this.state.grid)}</p>
+        <p>{JSON.stringify(this.state.grid)},</p>
         <label for="symmetrical-checkbox">symmetrical</label><input type="checkbox" id="symmetrical-checkbox" checked={this.state.symmetrical} onClick={() => this.setState({symmetrical: !this.state.symmetrical})}></input>
         <br/>
         <button onClick={() => this.trainBrain()}>Train network</button>
